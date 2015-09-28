b870dcc7f087228c3f824c4c7dc6b9c708794563
Josh Kosmala <josh@novaweb.co.nz>
2015-09-29 12:59:21 +1300

    Tenon API CURL settings

--- code/TenonProcessor.php
+++ code/TenonProcessor.php
@@ -27,18 +27,19 @@ class TenonProcessor extends Controller {
       // Initialise cURL
      $curlObj = curl_init();
      curl_setopt($curlObj, CURLOPT_URL, $tenon->TenonURL);
-     curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
+     //curl_setopt($curlObj, CURLOPT_SSL_VERIFYPEER, 0);
      curl_setopt($curlObj, CURLOPT_RETURNTRANSFER, 1);
      curl_setopt($curlObj, CURLOPT_POST, true);
      curl_setopt($curlObj, CURLOPT_FAILONERROR, true);
      curl_setopt($curlObj, CURLOPT_POSTFIELDS, $tenon_options);
-     curl_setopt($curlObj,CURLOPT_HTTPHEADER, array('Expect: '));
+     //curl_setopt($curlObj,CURLOPT_HTTPHEADER, array('Expect: '));
      // Execute post, get results, close connection
      $data = curl_exec($curlObj);
-     $code = curl_getinfo($curlObj, CURLINFO_HTTP_CODE);
+     //$code = curl_getinfo($curlObj, CURLINFO_HTTP_CODE);
      curl_close($curlObj);
 
-     Debug::show($data);die();
+     $result = json_decode($data, true);
+     Debug::show($result);die();
      // Evaluate response
     /* $out = ($code === 200);
      if ($out){
